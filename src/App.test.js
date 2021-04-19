02dcf0d077ed370f5f251a3af0c809d7c7300ac2
The CodingFreakj <pallavidapriya75@gmail.com>
2021-04-19 14:50:11 +0530

    second commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import React from "react";
+import { mount, configure, shallow } from "enzyme";
+import App from "./App";
+describe("App testing", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+  test("render the App header", () => {
+    expect(wrapper.find("h1").text()).toContain("This is app file");
+  });
 });
